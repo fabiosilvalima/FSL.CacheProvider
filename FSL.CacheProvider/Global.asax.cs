27cc6fd6220d6d0d5684c2db3d63eaf0a9d309f4
Fabio Silva Lima <fabiosilvalima@terra.com.br>
2019-03-11 13:33:52 -0300

    changing from Ninject to Simple Injector

--- FSL.CacheProvider/Global.asax.cs
+++ FSL.CacheProvider/Global.asax.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using FSL.CacheProvider.Caching;
+using SimpleInjector;
+using SimpleInjector.Integration.Web.Mvc;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +15,16 @@ namespace FSL.CacheProvider
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var container = new Container();
+
+            container.Register<ICache, HttpRuntimeCache>(Lifestyle.Singleton);
+            container.Register<ICacheProvider, FslCacheProvider>(Lifestyle.Singleton);
+
+            container.Verify();
+
+            DependencyResolver.SetResolver(
+               new SimpleInjectorDependencyResolver(container));
         }
     }
 }
